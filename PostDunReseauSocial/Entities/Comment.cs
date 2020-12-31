c96d971afcbad9d27b0b49e7df894583f53306c6
Mario Luiz Miranda <mario_chan15@hotmail.com>
2020-12-30 23:08:23 -0300

    Add project files.

---
+++ PostDunReseauSocial/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostDunReseauSocial.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ PostDunReseauSocial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using PostDunReseauSocial.Entities;
+
+namespace PostDunReseauSocial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Bon voyage!!!");
+            Comment c2 = new Comment("C'est cool...");
+
+            Post p1 = new Post(
+                DateTime.Parse("2018-06-21 13:05:44"),
+                "Voyager en Nouvelle-Zélande",
+                "Je vais visiter ce beau pays!",
+                12
+                );
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Bonsoir");
+            Comment c4 = new Comment("Que la force soit avec toi!");
+
+            Post p2 = new Post(
+                DateTime.Parse("2018-07-28 23:14:09"),
+                "Bonsoir les gens!",
+                "À demain",
+                7
+                );
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
